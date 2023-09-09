c2edf30c4b35b51c48e2398f7b44fb40f834d13d
Paranoid Android <113983506+paranoid-android12@users.noreply.github.com>
2023-09-09 19:53:15 +0800

    Add files via upload

---
+++ calculator/src/main/java/com/example/calculator/HelloApplication.java
@@ -0,0 +1,39 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    double x, y = 0;
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        scene.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+
+        scene.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
